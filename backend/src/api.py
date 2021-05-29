f42d9f142b9ae7ffd2af49b926ec2186d0779620
myportfolio-tech <eduardo@myportfolio.com>
2021-05-29 10:30:48 -0400

    error handlers completed

--- backend/src/api.py
+++ backend/src/api.py
@@ -106,17 +106,17 @@ def post_drink(payload):
     }), 200
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+# '''
+# @TODO implement endpoint
+#     PATCH /drinks/<id>
+#         where <id> is the existing model id
+#         it should respond with a 404 error if <id> is not found
+#         it should update the corresponding row for <id>
+#         it should require the 'patch:drinks' permission
+#         it should contain the drink.long() data representation
+#     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+#         or appropriate status code indicating reason for failure
+# '''
 
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
@@ -177,6 +177,42 @@ def delete_drink(payload, drink_id):
 Example error handling for unprocessable entity
 '''
 
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "not authorized"
+        }), 400
+
+
+@app.errorhandler(401)
+def not_authorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "not authorized"
+        }), 401
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return jsonify({
+        "success": False,
+        "error": 403,
+        "message": "forbidden"
+        }), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+        }), 404
+
+
 
 @app.errorhandler(422)
 def unprocessable(error):
@@ -187,24 +223,27 @@ def unprocessable(error):
     }), 422
 
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+@app.errorhandler(500)
+def server_error(error):
+    return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error"
+    }), 500
 
-'''
 
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
 
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+# https://knowledge.udacity.com/questions/124492
+
+@app.errorhandler(AuthError)
+def authentification_failed(error):
+    return jsonify({
+        "success": False,
+        "error": error.status_code,
+        "message": error.error
+        }), error.status_code
